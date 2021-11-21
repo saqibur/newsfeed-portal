d767ce814f7cf31ca302d9d6103c36739d040dea
saqibur <saqibur.rahman98@gmail.com>
2021-11-21 20:36:26 +0600

    Fix styling issues

--- users/views/homepage.py
+++ users/views/homepage.py
@@ -1,7 +1,10 @@
 from typing import Any
 
-from django import http
-from django.http import response
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
+from django.http.response import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 
@@ -10,13 +13,14 @@ from django.urls import reverse_lazy
 class HomeView(TemplateView):
     template_name = "users/home.html"
 
+
     def get(
         self,
-        request:  http.HttpRequest,
+        request:  HttpRequest,
         *args:    Any,
         **kwargs: Any
-    ) -> http.HttpResponse:
+    ) -> HttpResponse:
         if request.user.is_anonymous:
-            return response.HttpResponseRedirect(reverse_lazy('users:login'))
+            return HttpResponseRedirect(reverse_lazy('users:login'))
         else:
             return super().get(request, *args, **kwargs)
