ac6a372c40eff2d8675f3ad6d17bf7bd189982bb
saqibur <saqibur.rahman98@gmail.com>
2021-11-22 20:01:59 +0600

    Add index view and fix-up logout template

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
+from config.views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('archon/', admin.site.urls),
     url('user/', include('users.urls', namespace='user')),
 ]
