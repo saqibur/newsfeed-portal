b1783fdb2953a1cc0ae3152d994dbf0e80f61d68
saqibur <saqibur.rahman98@gmail.com>
2021-11-23 11:01:32 +0600

    Add email helper

---
+++ libraries/sendgrid_service/sendgrid_serivce.py
@@ -0,0 +1,21 @@
+from os import environ
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+_SEND_GRID_API_CLIENT: SendGridAPIClient = SendGridAPIClient(environ.get('SENDGRID'))
+
+def send_email(
+    from_email:   str,
+    to_email:     str,
+    subject:      str,
+    html_content: str,
+):
+    try:
+        _SEND_GRID_API_CLIENT.send(Mail(
+            from_email   = from_email,
+            to_emails    = to_email,
+            subject      = subject,
+            html_content = html_content,
+        ))
+    except Exception as _exn:
+        print("Failed to send email")
