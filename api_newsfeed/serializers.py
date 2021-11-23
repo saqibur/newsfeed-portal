b3ba116876e11e2b58891e43f10f220c697cf127
Saqibur Rahman <85487877+saqibur@users.noreply.github.com>
2021-11-23 17:42:14 +0600

    Merge PR: Add newsfeed api (#22)

---
+++ api_newsfeed/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from news.models.article import Article
+
+
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model  = Article
+        fields = ('__all__')
